f9eacd96fd40841bf296dc184b5c7ee29f4620f4
Dirk Dokter <dirk.dokter@atmosuav.com>
2018-03-19 10:18:03 +0100

    Add PointExtensions for WPF TouchPoints and Manipulators.

--- Mapsui.UI.Wpf/ExtensionMethods/PointExtensions.cs
+++ Mapsui.UI.Wpf/ExtensionMethods/PointExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Mapsui.UI.Wpf
 {
@@ -8,5 +11,33 @@ namespace Mapsui.UI.Wpf
         {
             return new Geometries.Point(point.X, point.Y);
         }
-    }
+
+        public static Geometries.Point ToMapsui(this TouchPoint point)
+        {
+            return new Geometries.Point(point.Position.X, point.Position.Y);
+        }
+
+        public static List<Geometries.Point> ToMapsui(this TouchPointCollection points)
+        {
+            var l = new List<Geometries.Point>();
+
+            foreach (var p in points)
+            {
+                l.Add(p.ToMapsui());
+            }
+
+            return l;
+        }
+
+        public static List<Geometries.Point> ToMapsui(this IEnumerable<TouchPoint> points)
+        {
+            return points.Select(p => p.ToMapsui()).ToList();
+        }
+
+
+        public static List<Geometries.Point> ToMapsui(this IEnumerable<IManipulator> manipulators, IInputElement relativeTo)
+        {
+            return manipulators.Select(manipulator => manipulator.GetPosition(relativeTo).ToMapsui()).ToList();
+        }
+}
 }
